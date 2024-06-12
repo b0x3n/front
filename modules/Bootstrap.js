d126a9d01e4af6f1121ae29c81d65542fe0b989f
b0x3n <b0x3n4dm1n@gmail.com>
2024-06-12 03:56:38 +0100

    Setting up JavaScript modules - webpack

--- modules/Bootstrap.js
+++ modules/Bootstrap.js
@@ -1,16 +1,32 @@
 /**********************************************************
  * front/modules/Bootstrap.js
- * 
+ *
+ * Here, we import the App.js module, this will import
+ * all of the core modules and initialise the system.
+ *  
  */
 
     import App from './App.js';
 
+    let     Main = () =>
+    {
+        let     app = App();
 
-    let     app = App();
-
-    if (app.isErr()) {
-        app.isErr(true);
-        console.log("Bailed due to error!");
-    }
-    else
+    /******************************************************
+     * Error check - app.isErr() will return true if
+     * any errors were recorded.
+     * 
+     */
+        if (app.isErr()) {
+            app.isErr(true);
+            return;
+        }
+    
+    /******************************************************
+     * All good, no errors.
+     */
         console.log('Ready!');
+    };
+
+
+    Main();
